9eb7193d774e58c55127f213001c68da34fe1375
Ulises Martinez Araiza <mx.ulises@gmail.com>
2024-02-06 23:03:02 -0800

    Distributed Services with Go - Chapter 5 (Part 3) - Add ACL authorization support in tests

--- distributed-services-with-go/proglog/internal/server/server_test.go
+++ distributed-services-with-go/proglog/internal/server/server_test.go
@@ -17,7 +17,9 @@ import (
 func TestServer(t *testing.T) {
 	for scenario, fn := range map[string]func(
 		t *testing.T,
-		client api.LogClient,
+		//client api.LogClient,
+		rootClient api.LogClient,
+		nobodyClient api.LogClient,
 		config *Config,
 	) {
 		"produce/consume a message to/from the log succeeds": testProduceConsume,
@@ -25,7 +27,8 @@ func TestServer(t *testing.T) {
 		"consume past log boundary fails": testConsumePastBoundary,
 	} {
 		t.Run(scenario, func(t *testing.T) {
-			client, config, teardown := setupTest(t, nil)
+			//client, config, teardown := setupTest(t, nil)
+			rootClient, nobodyClient, config, teardown := setupTest(t, nil)
 			defer teardown()
 			fn(t, client, config)
 		})
@@ -91,14 +94,17 @@ func setupTest(t *testing.T, fn func(*Config)) (
 
 	return client, cfg, func() {
 		server.Stop(),
-		cc.Close(),
+		//cc.Close(),
+		rootConn.Close(),
+		nobodyConn.Close(),
 		l.Close(),
 		//clog.Remove()
 	}
 }
 
-func testProduceConsume(t *testing.T, client api.Log.LogClient, config *Config) {
-	ctx := context.Background()
+//func testProduceConsume(t *testing.T, client api.Log.LogClient, config *Config) {
+func testProduceConsume(t *testing.T, client, _ api.Log.LogClient, config *Config) {
+		ctx := context.Background()
 
 	want := &api.Record{
 		Value: []byte("hello world"),
@@ -123,8 +129,9 @@ func testProduceConsume(t *testing.T, client api.Log.LogClient, config *Config) 
 	require.Equal(t, want.Offset, consume.Record.Offset)
 }
 
-func testConsumePastBoundary(t *testing.T, client api.LogClient, config *Config) {
-	ctx := context.Background()
+//func testConsumePastBoundary(t *testing.T, client api.LogClient, config *Config) {
+func testConsumePastBoundary(t *testing.T, client, _ api.LogClient, config *Config) {
+		ctx := context.Background()
 
 	produce, err := client.Produce(
 		ctx,
@@ -152,7 +159,8 @@ func testConsumePastBoundary(t *testing.T, client api.LogClient, config *Config)
 	}
 }
 
-func testProduceConsumeStream(t *testing.T, client api.LogClient, config *Config) {
+//func testProduceConsumeStream(t *testing.T, client api.LogClient, config *Config) {
+func testProduceConsumeStream(t *testing.T, client, _ api.LogClient, config *Config) {
 	ctx := context.Background()
 
 	records := []*api.Record{{
@@ -201,3 +209,35 @@ func testProduceConsumeStream(t *testing.T, client api.LogClient, config *Config
 		}
 	}
 }
+
+newClient := func(crtPath, keyPath string) (
+	*grpc.ClientConn,
+	api.LogClient,
+	[]grpc.DialOption,
+) {
+	tlsConfig, err := config.SetupTLSConfig(config.TLSConfig{
+		CertFile: crtPath,
+		KeyFile: keyPath,
+		CAFile: config.CAFile,
+		Server: false,
+	})
+	require.NoError(t, err)
+	tlsCreds := credentials.NewTLS(tlsConfig)
+	opts := []grpc.DialOption{grpc.WithTransportCredentials(tlsCreds)}
+	conn, err := grpc.Dial(l.Addr().String(), opts...)
+	require.NoError(t, err)
+	client := api.NewLogClient(conn)
+	return conn, client, opts
+}
+
+var rootConn *grpc.ClientConn
+rootConn, rootClient, _ = newClient(
+	config.RootClientCertFile,
+	config.RootClientKeyFile,
+)
+
+var nobodyConn *grpc.ClientConn
+nobodyConn, nobodyClient, _ = newClient(
+	config.NobodyClientCertFile,
+	config.NobodyClientKeyFile,
+)
