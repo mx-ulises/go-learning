d05a3f1ed10a6fd7f082ff0570635efecd755d31
Ulises Martinez Araiza <mx.ulises@gmail.com>
2024-03-05 22:09:06 -0800

    Leetcode: https://leetcode.com/problems/linked-list-cycle/

---
+++ leetcode/linked-list-cycle.go
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func hasCycle(head *ListNode) bool {
+	head = &ListNode{Next: head}
+	slow := head
+	fast := head.Next
+	for fast != nil && fast.Next != nil {
+		if slow == fast {
+			return true
+		}
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	return false
+}
