925d8fcf3443bf1ff01d91d35116eed5355af091
Ulises Martinez Araiza <mx.ulises@gmail.com>
2024-03-26 14:53:40 -0700

    Leetcode: https://leetcode.com/problems/first-missing-positive/ (new solutions)

--- leetcode/first-missing-positive.go
+++ leetcode/first-missing-positive.go
@@ -1,4 +1,4 @@
-func firstMissingPositive(nums []int) int {
+func firstMissingPositiveMemory(nums []int) int {
 	n := 100000
 	if len(nums) < n {
 		n = len(nums)
@@ -15,4 +15,37 @@ func firstMissingPositive(nums []int) int {
 		}
 	}
 	return -1
+}
+
+func GetNum(num, marker int) int {
+	for num < 0 {
+		num += marker
+	}
+	return num % marker
+}
+
+func firstMissingPositive(nums []int) int {
+	start := 1
+	end := len(nums)
+	marker := end + 1
+	// Remove numbers out of bounds
+	for i := 0; i < end; i++ {
+		if nums[i] < start || end < nums[i] {
+			nums[i] = 0
+		}
+	}
+	// Mark visited
+	for i := 0; i < end; i++ {
+		j := GetNum(nums[i], marker)
+		if j != 0 {
+			nums[j-1] -= marker
+		}
+	}
+	// Find the first integer not populated
+	for i := 0; i < end; i++ {
+		if 0 <= nums[i] {
+			return i + 1
+		}
+	}
+	return marker
 }
