ee41b3866ca3b4402bae42c1634dab1cbf4d712a
Ulises Martinez Araiza <mx.ulises@gmail.com>
2025-08-15 22:42:06 -0700

    Leetcode: https://leetcode.com/problems/sign-of-the-product-of-an-array/

---
+++ leetcode/sign-of-the-product-of-an-array.go
@@ -0,0 +1,14 @@
+package leetcode
+
+func arraySign(nums []int) int {
+	sign := 1
+	for _, num := range nums {
+		if num == 0 {
+			return 0
+		}
+		if num < 0 {
+			sign = -sign
+		}
+	}
+	return sign
+}
